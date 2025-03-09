b3b181e8fb4f1eff3db6a40b385a5ac2ee55acbc
code-with-naimish <naimish028@gmail.com>
2025-03-09 16:24:32 +0530

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,16 @@ function App() {
 
   return (
     <>
-      <RootNavs />
+      {/* <RootNavs /> */}
+      <div className='flex bg-gray-100 min-h-screen'>
+        <aside className='border-r border-gray-200 bg-white  min-w-64 fixed inset-y-0 flex flex-col'>fhfhf</aside>
+        <div className=' ml-64 flex-1  '>
+          <header className='flex  justify-between items-center gap-6 bg-blue-400 border-b border-gray-200 fixed top-0 left-64 right-0'>
+            <p>hgkd</p>
+            <p>hgkd</p>
+          </header>
+        </div>
+      </div>
     </>
   )
 }
