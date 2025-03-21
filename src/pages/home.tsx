dd29d49275b132902cf0d2c941cc52645849e1b1
code-with-naimish <naimish028@gmail.com>
2025-03-21 22:10:08 +0530

    react form initiated

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,43 @@
+import { useForm, SubmitHandler } from "react-hook-form"
 
-const Home = () => {
-  return (
-    <div className=" bg-amber-100">Home</div>
-  )
+
+type Inputs = {
+  name: string,
 }
 
-export default Home
+
+export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+
+
+
+
+  return (
+    <div className="bg-white rounded-lg p-5">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid sm:grid-cols-2 grid-cols-1 gap-4">
+          <div className="flex flex-col gap-2">
+            <label htmlFor="name">Name
+              <span className="ml-1 text-red-500">*</span>
+            </label>
+
+            <input className="w-full border border-gray-300 rounded-lg px-3 py-1" {...register("name", { required: true })} />
+            {errors.name && <span className="text-sm text-red-500">This field is required</span>}
+
+          </div>
+        </div>
+
+
+
+
+        <input type="submit" />
+      </form>
+    </div>
+  )
+}
